6c257ef54aba017db677287d17529cd9cd9ddab1
NindiRamadhani <nindiramadhani1010@gmail.com>
2026-04-01 00:29:18 +0700

    update tampilan dari Nindi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Exercise 3 React</h1>
+      
+      <div style={{ marginTop: '20px' }}>
+        <p>Ini Kita Kelompok "Terserah"💜💜💜💜</p>
+        <p>Ada Nindi</p>
+        <p>Ada Novi</p>
+        <p>Ada Yohana</p>
+      </div>
     </div>
   );
 }
